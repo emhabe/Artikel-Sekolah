490367364425508bc39a93dc2b4570a67c0949a8
agung1012 <108931942+agung1012@users.noreply.github.com>
2022-12-10 07:35:55 +0700

    index_cuman_satu1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ArtikelController;
 
 /*
 |--------------------------------------------------------------------------
