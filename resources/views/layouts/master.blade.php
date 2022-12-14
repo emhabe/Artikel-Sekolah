32249177a01809e48d0eeea152eb5a3d48b5b9d8
ivran6969 <achmadivransantoso@gmail.com>
2022-12-14 21:20:44 +0700

    pegel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,7 +74,7 @@
   <!-- beautify ignore:start -->
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
   <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-  <script src="{{asset('assets/vendor/js/template-customizer.js')}}"></script>
+  <!-- <script src="{{asset('assets/vendor/js/template-customizer.js')}}"></script> -->
 
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
   <script src="{{asset('assets/js/config.js')}}"></script>
